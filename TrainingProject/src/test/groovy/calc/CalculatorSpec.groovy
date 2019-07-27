9a35dc6043f7e696a1ca3e9d0039b56545a08120
@jit@12345 <ajit-nalawade>
2019-07-27 11:54:14 -0500

    Test case to add two numbers seperated by comma

--- TrainingProject/src/test/groovy/calc/CalculatorSpec.groovy
+++ TrainingProject/src/test/groovy/calc/CalculatorSpec.groovy
@@ -6,15 +6,24 @@ import spock.lang.Specification
 class CalculatorSpec extends Specification{
 
 	@Shared Calculator calc = new Calculator();
+	
 	def 'API to add two numbers'(){
-		given : 'String conatining two numbers'
+		given : 'String conatining blank'
+			def numbers = ''
 		when: 'Calculator.add is called'
-			int result = calc.add(numbers)
-		then:
-			result == expected	
-		where:
-			numbers| expected
-			''	|	0
+			def result = calc.add(numbers)
+		then: 'It should return 0'
+			result == 0	
 	}
+	
+	def 'API to add two numbers - test with two numbers'(){
+		given : 'String containing two numbers'
+			def numbers = '2,3'
+		when : 'Calculator.add is called'
+			def result = calc.add(numbers)
+		then : 'It should return addtion of 2 & 3 i.e 5'
+			result == 5	
+	}
+	
 }
 
