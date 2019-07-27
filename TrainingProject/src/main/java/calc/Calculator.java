1f5b2b497c827b6958f58876c034451ad7cf6302
@jit@12345 <ajit-nalawade>
2019-07-27 11:28:36 -0500

    Test Case for blank input

--- TrainingProject/src/main/java/calc/Calculator.java
+++ TrainingProject/src/main/java/calc/Calculator.java
@@ -4,9 +4,12 @@ public class Calculator {
 
 	int add(String numbers)
 	{
-		int result;
-		String [] number = numbers.split(",");
-		result = Integer.parseInt(number[0]) + Integer.parseInt(number[1]);
+		int result = 0;
+		if(!"".equals(numbers))
+		{
+			String [] number = numbers.split(",");
+			result = Integer.parseInt(number[0]) + Integer.parseInt(number[1]);
+		}
 		return result;
 	}
 }
